3916d87fa34f2beb8c9f82609560e6fbda52aad1
ravi_ranjan <rranjankumarprajapati@gmail.com>
2023-06-19 08:52:36 +0530

    Changes to be committed: 	modified:   database.py

--- database.py
+++ database.py
@@ -9,12 +9,6 @@ class Datbase:
 
         try:
             if musername == self.__uname and mpassword == self.__upass:
-                # db = mysql.connector.connect(host='b9hhck7wnz51bsehyc7w-mysql.services.clever-cloud.com',
-                # database='b9hhck7wnz51bsehyc7w',
-                # user='ufn0kwnys97w7zmr',
-                # port='3306',
-                # password='72aR1zfrW8RlIxfahZTH',
-                # )
 
                 db = mysql.connector.connect(host='localhost', user='root', password='ubuntu')
 
