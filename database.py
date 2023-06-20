844d1a659a90218e332c1d7dffb9ed66c25d4ede
ravi_ranjan <rranjankumarprajapati@gmail.com>
2023-06-20 14:43:01 +0530

    modified:   database.py 	modified:   main.py 	modified:   static/styles/ext.css 	new file:   templates/emplogin.html 	renamed:    templates/dashboard.html -> templates/udashboard.html 	renamed:    templates/login.html -> templates/userlogin.html
    Changes not staged for commit:
    	modified:   main.py

--- database.py
+++ database.py
@@ -1,21 +1,24 @@
 from sqlalchemy import create_engine, text
 
-engine = create_engine("mysql://root:ubuntu@localhost/university",echo = True)
+engine = create_engine("mysql://root:ubuntu@localhost/bank",echo = True)
 
-def load_faculty():
+def check_ulogin(uname, upass):
     with engine.connect() as conn:
-        result = conn.execute(text("select * from faculty"))
+        result = conn.execute(text("select * from account_login where Account_number = :acc"),acc=uname)
+        result = result.all()
+        if len(result) > 0 and upass == result[0][1]:
+            return True
+        else:
+            return False
+    
+def acc_details(acc_no):
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from accounts where Account_number = :acc"),acc=acc_no)
         return result.all()
     
-def load_student():
+def branch_details(branch_code):
     with engine.connect() as conn:
-        result = conn.execute(text("select * from students"))
+        result = conn.execute(text("select * from branch where Branch_code = :branch_code"),branch_code=branch_code)
         return result.all()
+    
 
-def load_student(dept):
-    with engine.connect() as conn:
-        result = conn.execute(text("select * from students where dept = :val"),val=dept)
-        return result.all()
-
-
-        
--- main.py
+++ main.py
@@ -1,30 +1,56 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for, g
+import os
 from database import *
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
-print(load_student(dept='phy'))
 
-# @app.route("/")
-# def index():
-#     return render_template('index.html',query=load_faculty())
+@app.route('/', methods=['POST','GET'])
+def userlogin():
+
+  if request.method == 'POST':
+    session.pop('user',None)
+
+    if check_ulogin(request.form['uname'],request.form['upass']):
+      session['user'] = request.form['uname']
+      return redirect(url_for('udashboard'))
+    
+  return render_template('userlogin.html')
 
 @app.route("/dashboard")
-def student():
-    return render_template('dashboard.html')
+def udashboard():
+    if g.user:
+      adetails = acc_details(session['user'])
+      bdetails = branch_details(adetails[0][6])
+      return render_template('udashboard.html',adetails=adetails,bdetails=bdetails)
+    else:
+       return redirect(url_for('userlogin'))
+       
 
 # @app.route("/student")
 # def student():
 #     return render_template('student.html', quer=load_student(dept='phy'))
 
-@app.route('/')
-def new_accounts():
-    return render_template('login.html')
+@app.route('/admin')
+def emplogin():
+    return render_template('emplogin.html')
 
 @app.route('/acc', methods=['post'])
 def accounts():
     # data = request.args
     data = request.form
     return jsonify(data)
+
+@app.before_request
+def before_request():
+  g.user = None
+  if 'user' in session:
+    g.user = session['user']
+
+@app.route('/dropsession')
+def dropsession():
+  session.pop('user',None )
+  return redirect(url_for('login'))
 
 app.run(debug=True)
