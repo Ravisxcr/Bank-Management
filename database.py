12302b5a0e78490f5382c85050b627dc29967de4
ravi_ranjan <rranjankumarprajapati@gmail.com>
2023-06-21 10:31:51 +0530

    Changes to be committed: 	modified:   database.py 	modified:   main.py 	deleted:    requirements.txt 	modified:   templates/accopen.html 	renamed:    templates/emplogin.html -> templates/deposit.html 	new file:   templates/edashboard.html 	new file:   templates/message.html 	modified:   templates/udashboard.html 	modified:   templates/userlogin.html 	new file:   templates/utransaction.html

--- database.py
+++ database.py
@@ -21,4 +21,67 @@ def branch_details(branch_code):
         result = conn.execute(text("select * from branch where Branch_code = :branch_code"),branch_code=branch_code)
         return result.all()
     
+def acc_tranaction(acc_no):
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from tranaction where Account_number = :acc order by tr_time desc"),acc=acc_no)
+        return result.all()
+    
+def ubalance(acc_no):
+    with engine.connect() as conn:
+        result = conn.execute(text("select Balance from accounts where Account_number = :acc"),acc=acc_no)
+        return result.all()
+    
+def user_pay(data):
+    print(data)
+    if check_ulogin(data['uname'],data['upass']): 
+        current_bal = ubalance(data['uname'])[0][0]
+        if  current_bal> int(data["amount"]) + 50:
+            with engine.connect() as conn:
+                new_bal = current_bal-int(data['amount'])
+                result = conn.execute(text("insert into tranaction (Tr_amount,  Tr_type,  Account_number,  tr_info, tr_balance) values (:amt, 'D', :acc, :trinfo, :trbal)"),amt=data['amount'],acc=data['uname'], trinfo='ONLINE_PAID_TO_'+str(data['payee']), trbal=new_bal)
+
+                result = conn.execute(text('update accounts set Balance = :bal where  Account_number = :acc_no'),bal=new_bal, acc_no=data['uname'])
+            return 'S'
+        else:
+            return 'IB'
+    else:
+        return 'F'
+
+
+def check_elogin(uname, upass):
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from employee where Emp_number = :emp_no"),emp_no=uname)
+        result = result.all()
+        if len(result) > 0 and str(upass) == result[0][6]:
+            return True
+        else:
+            return False
+
+
+def acc_emp(emp_no):
+    emp_det , br_detail = None, None
+    with engine.connect() as conn:
+        emp_det = conn.execute(text("select Emp_number, Emp_name, Gender, Address, Designation, Branch_code from employee where Emp_number = :emp_no"),emp_no=emp_no)
+        emp_det = emp_det.all()
+        if len(emp_det) > 0:
+            br_detail = branch_details(emp_det[0][5])
+        
+    return emp_det, br_detail
+        
+
+def all_tr():
+    with engine.connect() as conn:
+        tr = conn.execute(text("select * from tranaction order by tr_time desc"))
+        return tr.all()
+
+def all_customer():
+    with engine.connect() as conn:
+        query = conn.execute(text('select * from accounts'))
+        return query.all()
+
+def br_customer(br_code):
+    with engine.connect() as conn:
+        query = conn.execute(text('select * from accounts where Branch_code = :br_code'),br_code=br_code)
+        return query.all()
+
 
--- main.py
+++ main.py
@@ -1,40 +1,75 @@
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for, g
 import os
 from database import *
+import json
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
+# User Login Page
 
 @app.route('/', methods=['POST','GET'])
 def userlogin():
-
   if request.method == 'POST':
     session.pop('user',None)
 
-    if check_ulogin(request.form['uname'],request.form['upass']):
+    if request.form['acc_type'] == 'Customer' and check_ulogin(request.form['uname'],request.form['upass']):
       session['user'] = request.form['uname']
       return redirect(url_for('udashboard'))
     
+    elif request.form['acc_type'] == 'Employee' and check_elogin(request.form['uname'],request.form['upass']):
+      session['user'] = request.form['uname']
+      return redirect(url_for('edashboard'))
+    
   return render_template('userlogin.html')
 
+# User Dashboard
 @app.route("/dashboard")
 def udashboard():
     if g.user:
       adetails = acc_details(session['user'])
       bdetails = branch_details(adetails[0][6])
-      return render_template('udashboard.html',adetails=adetails,bdetails=bdetails)
+      trdetails = acc_tranaction(session['user'])
+      return render_template('udashboard.html',adetails=adetails,bdetails=bdetails,trdetails=trdetails)
     else:
        return redirect(url_for('userlogin'))
+    
+@app.route("/employee")
+def edashboard():
+  if g.user:
+    emp_det, br_detail = acc_emp(session['user'])
+    tr_detail = all_tr()
+    return render_template('edashboard.html',emp_det=emp_det, br_detail=br_detail, tr_detail=tr_detail)
+  else:
+    return redirect(url_for('userlogin')) 
        
-
-@app.route('/admin')
-def emplogin():
-    return render_template('emplogin.html')
-
-@app.route('/acc')
+ 
+# Account Opening Form
+@app.route('/newacc',methods=['POST','GET'])
 def accounts():
+  if g.user:
     return render_template('accopen.html')
+  else:
+    return redirect(url_for('userlogin'))
+
+
+# 
+@app.route('/transaction',methods=['POST','GET'])
+def utranaction():
+  return render_template('utransaction.html')
+
+@app.route('/deposit',methods=['POST','GET'])
+def deposit():
+  return render_template('deposit.html')
+
+
+
+@app.route('/data', methods=['GET','POST'])
+def data():
+  return jsonify(request.form)
+
+
+
 
 @app.before_request
 def before_request():
@@ -45,6 +80,6 @@ def before_request():
 @app.route('/dropsession')
 def dropsession():
   session.pop('user',None )
-  return redirect(url_for('login'))
+  return redirect(url_for('userlogin'))
 
 app.run(debug=True)
